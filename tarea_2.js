b4c5f8c30c4ea60b8ca5dbfcaea7586fdc0de66e
WesleyRivas <wesleyriv15@gmail.com>
2020-02-09 14:48:27 -0600

    Unidad 3, Tarea 1

---
+++ tarea_2.js
@@ -0,0 +1,14 @@
+var mes = 10
+
+  if (mes == 12 || mes == 1 || mes== 2){
+    document.write("Es invierno");
+  } else if (mes >= 3 && mes <= 5){
+    document.write("Es Primavera");
+  } else if (mes >=6 && mes <=8){
+    document.write("Es Verano");
+  } else if (mes >= 9 && mes <= 11){
+    document.write("Es Otono");
+  } else {
+    document.write("Ese mes no existe");
+  }
+  document.write("<br>Wesley Danilo Rivas Rivas <br> 18005496")
